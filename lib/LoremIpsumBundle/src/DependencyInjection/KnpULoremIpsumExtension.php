70e45fe09cae32dab5648bcc7aa034d80811f1d0
Jurij Leshchuk <ddevdev609@gmail.com>
2023-11-01 20:16:32 +0200

    All about the Bundle Extension Config System

--- lib/LoremIpsumBundle/src/DependencyInjection/KnpULoremIpsumExtension.php
+++ lib/LoremIpsumBundle/src/DependencyInjection/KnpULoremIpsumExtension.php
@@ -12,6 +12,7 @@ class KnpULoremIpsumExtension extends Extension
 
     public function load(array $configs, ContainerBuilder $container)
     {
+        var_dump($configs);
         $loader = new XmlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
         $loader->load('services.xml');
     }
